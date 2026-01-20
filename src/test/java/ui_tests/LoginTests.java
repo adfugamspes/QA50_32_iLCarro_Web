53e49eae719f0bb891b953253cbe300834959da5
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-01-20 21:02:19 +0200

    CW5

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,8 +1,11 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends AppManager {
 
@@ -10,5 +13,16 @@ public class LoginTests extends AppManager {
     public void loginPositiveTest (){
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogIn();
+    }
+
+    @Test
+    public void loginPositiveTestWithUser (){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogIn();
+        User user = User.builder().email("correctmail123@mail.com").password("Password123!").build();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm(user);
+        loginPage.clickBtnYalla();
+        Assert.assertTrue(loginPage.isLoggedInDisplayed());
     }
 }
