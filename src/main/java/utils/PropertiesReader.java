4e934935a6f6c0e074f05dfb000402d9687f21de
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-02-04 14:41:38 +0200

    HW8 part 4

--- src/main/java/utils/PropertiesReader.java
+++ src/main/java/utils/PropertiesReader.java
@@ -1,4 +1,25 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesReader {
+
+    public static String getProperty(String fileName, String key) {
+        Properties properties = new Properties();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("/src/test/resources/properties/"
+                    + File.separator + fileName);
+            properties.load(fileInputStream);
+            return properties.getProperty(key);
+        } catch (IOException e) {
+            System.out.println("Exception created");
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
