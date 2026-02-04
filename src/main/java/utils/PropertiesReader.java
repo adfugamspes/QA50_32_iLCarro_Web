6cebcc00b7d9c19b3a9ab776a9ad50b457e0c1d6
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-02-04 14:43:59 +0200

    HW8 part 5

--- src/main/java/utils/PropertiesReader.java
+++ src/main/java/utils/PropertiesReader.java
@@ -11,7 +11,7 @@ public class PropertiesReader {
     public static String getProperty(String fileName, String key) {
         Properties properties = new Properties();
         try {
-            FileInputStream fileInputStream = new FileInputStream("/src/test/resources/properties/"
+            FileInputStream fileInputStream = new FileInputStream("src/test/resources/properties"
                     + File.separator + fileName);
             properties.load(fileInputStream);
             return properties.getProperty(key);
