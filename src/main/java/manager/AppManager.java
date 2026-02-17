8e7ce6b3913c7fc6e353c43a467101a75d4bd685
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-02-17 11:36:51 +0200

    HW 9 part2

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -1,15 +1,23 @@
 package manager;
 
+import dto.User;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.PopUpPage;
+import utils.enums.HeaderMenuItem;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import static utils.UserFactory.positiveUserLogin;
 
 public class AppManager {
     public final static Logger logger = LoggerFactory.getLogger(AppManager.class);
--- src/test/java/ui_tests/SearchCarTests.java
+++ src/test/java/ui_tests/SearchCarTests.java
@@ -3,6 +3,7 @@ package ui_tests;
 import dto.User;
 import manager.AppManager;
 import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -10,32 +11,24 @@ import pages.FindCarSearchResults;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.PopUpPage;
+import utils.enums.HeaderMenuItem;
 
 import static utils.UserFactory.*;
 
 public class SearchCarTests extends AppManager {
     HomePage homePage;
-    LoginPage loginPage;
-    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
-    public void goToLogInPage(){
+    public void goToHomePage(){
         homePage = new HomePage(getDriver());
-        homePage.clickBtnLogIn();
-        loginPage = new LoginPage(getDriver());
-        User user = positiveUserLogin();
-        loginPage.typeLoginForm(user);
-        loginPage.clickBtnYalla();
-        new PopUpPage(getDriver()).clickBtnOk();
     }
 
     @Test
     public void checkElements(){
-       new HomePage(getDriver()).typeFindCarForm("Tel-Aviv", "2/18/2026 - 2/21/2026");
+       homePage.typeFindCarForm("Tel-Aviv", "2/18/2026 - 2/21/2026");
        homePage.activateAndClickBtnYalla();
        FindCarSearchResults findCarSearchResults = new FindCarSearchResults(getDriver());
        Assert.assertTrue(findCarSearchResults.isTextInSearchResultsMessagePresents("No available cars"));
     }
-
 
 }
